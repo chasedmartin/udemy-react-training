b3ee8c0710b470d9979ef31b4eaec6a65de64c0c
Chase Martin <chasedmartin@gmail.com>
2020-05-23 13:22:11 -0500

    Up to "36. Working with Props"

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
-    return (
+     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hi, I'm a react app</h1>
+        <Person name="Chase" age="30" />
+        <Person name="Richard" age="34">My hobbies: nerd stuff</Person>
+        <Person name="Virag" age="23" />
       </div>
     );
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
