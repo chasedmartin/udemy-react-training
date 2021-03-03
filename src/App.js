e8f703ef9475c7f1a75cdd4668cf8d2a9f2c0617
Chase Martin <chasedmartin@gmail.com>
2021-03-03 16:21:10 -0600

    60. Flexible Lists

--- src/App.js
+++ src/App.js
@@ -20,12 +20,21 @@ class App extends Component {
     this.setState({persons: persons});
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({persons: [
-      { name: 'Chase', age: 30},
-      { name: event.target.value, age: 34},
-      { name: 'Virag', age: 24}
-    ]})
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons:persons});
   }
 
   togglePersonsHandler = () => {
@@ -52,7 +61,8 @@ class App extends Component {
               click={() => this.deletePersonHandler(index)}
               name={person.name}
               age={person.age}
-              key={person.id} />
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)} />
           })}
           </div>
       );
